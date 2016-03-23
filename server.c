0b3fb1ea6eddf4dd04cb2732df1d8974bd460f86
JOUANNET Rémi <remi.jouannet@neurones.net>
2016-03-23 19:03:27 +0100

    yolo

--- server.c
+++ server.c
@@ -13,7 +13,7 @@
 #include "cpu_usage.h"
 #include "mem_info.h"
 
-#define BUFFER 1500
+#define BUFFER 500
 
 int socket_server(int port){
     int sockfd, new_fd, pid;
@@ -22,7 +22,7 @@ int socket_server(int port){
     unsigned int sin_size;
 
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0))==-1){
-        printf("create socket error");
+        printf("create socket error\n");
         exit(1);
     }
 
@@ -34,12 +34,12 @@ int socket_server(int port){
     my_addr.sin_addr.s_addr = INADDR_ANY;
 
     if(bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))==-1){
-        printf("bind socket error");
+        printf("bind socket error\n");
         exit(1);
     }
 
     if(listen(sockfd, 10)==-1){
-        printf("listen");
+        printf("listen\n");
         exit(1);
     }
 
@@ -49,7 +49,7 @@ int socket_server(int port){
         printf("server is run %i\n", port);
 
         if((new_fd = accept(sockfd, &their_addr, &sin_size)) < 0){
-            printf("accept");
+            printf("accept\n");
             exit(1);
         }
         
@@ -82,34 +82,39 @@ int while_interface(int sockfd){
 
     uint8_t buffer[BUFFER] = {0};
     intmax_t len;
-	fd_set fdset;
-
+    double cpu_usage;
+    double mem_usage;
+    double mem_total;
 	
     while(1){
-		FD_ZERO(&fdset);
-		FD_SET(sockfd, &fdset);
-		
-        if (select(sockfd+1, &fdset, NULL, NULL, NULL) < 0){
-            printf("select");
-            exit(1);
-        } 
-        
         memset(&buffer, 0, sizeof(buffer));
 
-         if(FD_ISSET(sockfd, &fdset)) {
-            len = recv(sockfd, buffer, sizeof(buffer), 0);
+        len = recv(sockfd, buffer, sizeof(buffer), 0);
 
-            if( len <= 0 ){
-                close(sockfd);
-                printf("recv error");
-                break;
-            }
+        printf("%lu %s \n", len, buffer);
 
-			if (send(sockfd, buffer ,sizeof(buffer),0) < 0){ 
-                printf("send error");
-                exit(1);
-            }
-		}
+        if( len <= 0 ){
+            close(sockfd);
+            printf("recv error\n");
+            exit(1);
+        }
+
+        if(memcmp(buffer, "1", 1) == 0){
+            mem_usage = getMemoryUsage();
+            len = sprintf((char*)buffer, "1 %f", mem_usage);
+        }else if(memcmp(buffer, "2", 1) == 0){
+            cpu_usage = getCurrentValue();
+            len = sprintf((char*)buffer, "2 %f", cpu_usage);
+        }else if(memcmp(buffer, "3", 1) == 0){
+            mem_total = getMemoryTotal();
+            len = sprintf((char*)buffer, "3 %f", mem_total);
+        }
+
+        if (send(sockfd, buffer, len, 0) < 0){ 
+            close(sockfd);
+            printf("send error\n");
+            exit(1);
+        }
     }
     return 0;
 }
